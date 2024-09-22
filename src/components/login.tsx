45209dae627807b18abefc4d43e436b6ade64ff2
Allen John <allenjohnmonapallil@gmail.com>
2024-09-22 13:15:12 +0530

    Login Lottie change

--- src/components/login.tsx
+++ src/components/login.tsx
@@ -27,15 +27,15 @@ export default function LoginComponent() {
           <div className="flex flex-col mx-auto max-w-lg w-full py-6 md:py-10 px-4 md:px-5 animate-fade-in">
             <div className="space-y-1">
               <h1 className="text-3xl font-bold text-center text-green-500">Cubing Kerala Login</h1>
-              <p className="text-neutral-500 text-sm text-center">Login with World Cube Association</p>
+              <p className="text-neutral-500 text-sm text-center">Authenticate with World Cube Association</p>
             </div>
             <div>
-              <div className="flex items-center justify-center w-full"><LoginLotttieComponent path="/login.json" /></div>
+              <div className="flex items-center justify-center h-[300px] w-full"><LoginLotttieComponent path="/login2.json" /></div>
               <Card className="border-none rounded-none bg-black text-stone-200">
                 <CardFooter className="flex items-center justify-center">
                   <ShimmerButton
                     onClick={handleLogin}
-                    className="text-green-400 font-semibold w-full md:w-2/3"
+                    className="text-green-400 font-semibold w-full md:w-2/3 h-10"
                     disabled={loading}
                   >
                     {loading ? (
