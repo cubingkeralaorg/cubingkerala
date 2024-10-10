f61e2a07ceba069a163e14507207ce123ae2556c
Allen John <allenjohnmonapallil@gmail.com>
2024-10-10 12:57:01 +0530

    Hover, text size adjustments

--- src/components/ck-footer.tsx
+++ src/components/ck-footer.tsx
@@ -1,15 +1,23 @@
 import Link from 'next/link'
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+
 
 
 const CubingKeralaFooter = () => {
     return (
         <footer className="py-6 px-4 sm:px-6 text-stone-400 flex justify-between items-center">
             <div className="space-y-1 w-full">
-                <p className="text-xs text-stone-600">Created by <Link href="https://github.com/allenjohn07" className="hover:underline hover:underline-offset-2 cursor-pointer hover:text-blue-500">Allen</Link></p>
+                <p className="text-xs text-stone-600">Created by <Link href="https://github.com/allenjohn07" className="cursor-pointer hover:text-blue-500">allenjohn</Link></p>
                 <div className='flex items-center justify-between'>
                     <p className="text-xs">&copy; {new Date().getFullYear()} Cubing Kerala. All rights reserved.</p>
-                    <Link href="https://github.com/cubingkeralaorg/cubingkerala"><p className='text-stone-600 hover:text-blue-500 hover:underline hover:underline-offset-2 cursor-pointer text-xs hidden md:block'>Github</p></Link>
+                    <Link href="https://github.com/cubingkeralaorg/cubingkerala">
+                        <p className='text-stone-600 hover:text-blue-500 cursor-pointer text-xs hidden md:flex gap-1 items-center'>
+                            <FaGithub className='text-xs' />
+                            github
+                        </p>
+                        <FaGithub className='text-stone-600 hover:text-blue-500 cursor-pointer text-xs md:hidden mr-1' />
+                    </Link>
                 </div>
             </div>
         </footer>
