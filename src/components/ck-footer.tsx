a097ee86e78fde71de468c366ad77530dd7232b7
Allen John <allenjohnmonapallil@gmail.com>
2024-10-23 14:00:35 +0530

    Opening Footer Links in new tab

--- src/components/ck-footer.tsx
+++ src/components/ck-footer.tsx
@@ -1,23 +1,31 @@
-import Link from 'next/link'
+'use client'
+
 import React from 'react'
 import { FaGithub } from "react-icons/fa";
 
+
+const handleLinkRedirect = () => {
+    window.open("https://www.allenjohn.online", "_blank")
+}
+
+
+const handleGithubRedirect = () => {
+    window.open("https://github.com/cubingkeralaorg/cubingkerala", "_blank")
+}
 
 
 const CubingKeralaFooter = () => {
     return (
         <footer className="py-6 px-4 sm:px-6 text-stone-400 flex justify-between items-center">
             <div className="space-y-1 w-full">
-                <p className="text-xs text-stone-600">Created by <Link href="https://www.allenjohn.online" className="cursor-pointer hover:text-blue-500">allenjohn</Link></p>
+                <p className="text-xs text-stone-600">Created by <span onClick={() => handleLinkRedirect()} className="cursor-pointer hover:text-blue-500">allenjohn</span></p>
                 <div className='flex items-center justify-between'>
                     <p className="text-xs">&copy; {new Date().getFullYear()} Cubing Kerala. All rights reserved.</p>
-                    <Link href="https://github.com/cubingkeralaorg/cubingkerala">
-                        <p className='text-stone-600 hover:text-blue-500 cursor-pointer text-xs hidden md:flex gap-1 items-center'>
-                            <FaGithub className='text-xs' />
-                            github
-                        </p>
-                        <FaGithub className='text-stone-600 hover:text-blue-500 cursor-pointer text-xs md:hidden mr-1' />
-                    </Link>
+                    <div onClick={() => handleGithubRedirect()} className='text-stone-400 hover:text-blue-500 cursor-pointer text-xs hidden md:flex gap-1 items-center'>
+                        <FaGithub className='text-xs' />
+                        <span className='pb-1'>github</span>
+                    </div>
+                    <FaGithub className='text-stone-400 hover:text-blue-500 cursor-pointer text-xs md:hidden mr-1' />
                 </div>
             </div>
         </footer>
