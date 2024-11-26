3ee75987c2f71b018566f08661321c3ce8526472
Allen John <allenjohnmonapallil@gmail.com>
2024-11-27 01:15:49 +0530

    Userinfo fix

--- src/components/alert.tsx
+++ src/components/alert.tsx
@@ -12,7 +12,7 @@ export function AlertComponent({ userInfo }: { userInfo: UserInfo | null }) {
     return (
         <>
             {
-                
+                userInfo &&
                 <div className="relative">
                     <Alert className="rounded-full absolute top-2 left-3 md:left-0 md:top-4 w-fit bg-black/55 border-none flex z-50 text-neutral-300">
                         <div>
