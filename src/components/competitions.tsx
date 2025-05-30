788594e2960fa659d6817323101b8ba8742c2827
Allen John <allenjohnmonapallil@gmail.com>
2025-05-30 15:44:34 -0600

    competition loading

--- src/components/competitions.tsx
+++ src/components/competitions.tsx
@@ -1,14 +1,30 @@
-import axios from "axios"
-import UpPastCompetitions from "./up-past-competitions";
-export const dynamic = 'force-dynamic'
+'use client';
 
-export default async function CompetitionsComponent() {
+import { useEffect, useState } from 'react';
+import axios from 'axios';
+import dynamic from 'next/dynamic';
+import LoadingComponent from './loading';
 
-  const response = await axios.get("https://www.worldcubeassociation.org/api/v0/competitions?country_iso2=IN&per_page=1000")
-  
+
+const UpPastDynamicComponent = dynamic(() => import('../components/up-past-competitions'), {
+  ssr: false,
+});
+
+export default function CompetitionsComponent({response}: { response: any }) {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setData(response);
+    setLoading(false);
+    setLoading(false);
+  }, []);
+
+  if (loading) return <div className="text-stone-400"><LoadingComponent/></div>;
+
   return (
     <div className="w-full bg-neutral-950 mx-auto py-6 md:py-8 px-4 md:px-5 text-stone-200">
-      <UpPastCompetitions response={response.data} />
+      <UpPastDynamicComponent response={data} />
     </div>
-  )
+  );
 }
