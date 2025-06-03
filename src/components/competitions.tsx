a77305d68c5caf8e0a4e23cb34e83270da3982b0
Allen John <allenjohnmonapallil@gmail.com>
2025-06-03 10:07:05 -0600

    Competitions to localStorage

--- src/components/competitions.tsx
+++ src/components/competitions.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { useEffect, useState } from 'react';
-import axios from 'axios';
 import dynamic from 'next/dynamic';
 import LoadingComponent from './loading';
 
@@ -10,12 +9,10 @@ const UpPastDynamicComponent = dynamic(() => import('../components/up-past-compe
   ssr: false,
 });
 
-export default function CompetitionsComponent({response}: { response: any }) {
-  const [data, setData] = useState(null);
+export default function CompetitionsComponent() {
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    setData(response);
     setLoading(false);
     setLoading(false);
   }, []);
@@ -24,7 +21,7 @@ export default function CompetitionsComponent({response}: { response: any }) {
 
   return (
     <div className="w-full bg-neutral-950 mx-auto py-6 md:py-8 px-4 md:px-5 text-stone-200">
-      <UpPastDynamicComponent response={data} />
+      <UpPastDynamicComponent />
     </div>
   );
 }
