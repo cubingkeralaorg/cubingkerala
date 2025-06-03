35aa7cc56c4f5bc2f945f168d344ed6b50292a37
Allen John <allenjohnmonapallil@gmail.com>
2025-06-03 13:26:49 -0600

    NavClose on request click

--- src/components/heroUiNavbar.tsx
+++ src/components/heroUiNavbar.tsx
@@ -112,7 +112,7 @@ export default function HeroUiNavbar() {
           {userInfo?.me?.id == 6996 ? (
             <Link
               className={
-                pathname == "/contact"
+                pathname == "/requests"
                   ? "text-green-400"
                   : "text-stone-200 hover:text-green-400 transition-all ease-in duration-200text-stone-200"
               }
