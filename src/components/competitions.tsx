d5c5d5ebf849b424ccfc7909dbf1231cc28b35d5
Allen John <allenjohnmonapallil@gmail.com>
2025-06-03 15:03:58 -0600

    remove: unwanted loading

--- src/components/competitions.tsx
+++ src/components/competitions.tsx
@@ -1,8 +1,6 @@
 'use client';
 
-import { useEffect, useState } from 'react';
 import dynamic from 'next/dynamic';
-import LoadingComponent from './loading';
 
 
 const UpPastDynamicComponent = dynamic(() => import('../components/up-past-competitions'), {
@@ -10,14 +8,6 @@ const UpPastDynamicComponent = dynamic(() => import('../components/up-past-compe
 });
 
 export default function CompetitionsComponent() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    setLoading(false);
-    setLoading(false);
-  }, []);
-
-  if (loading) return <div className="text-stone-400"><LoadingComponent/></div>;
 
   return (
     <div className="w-full bg-neutral-950 mx-auto py-6 md:py-8 px-4 md:px-5 text-stone-200">
