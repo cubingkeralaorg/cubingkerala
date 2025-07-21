b50bacc83281fbeff0dbf96179c6a0985274612a
Allen John <146406094+allenjohn07@users.noreply.github.com>
2025-07-21 12:25:03 -0600

    update: upComp reverse (#126)
    * update: upComp reverse
    
    * fix: accident space

--- src/app/api/get-competitions/route.ts
+++ src/app/api/get-competitions/route.ts
@@ -6,7 +6,7 @@ export async function GET(request: NextRequest) {
     try {
         const WCAIndianCompetitions = await axios.get("https://www.worldcubeassociation.org/api/v0/competitions?country_iso2=IN&per_page=1000")
         const KeralaCompetitions = WCAIndianCompetitions.data.filter((competitions: any) => competitions.city.includes("Kerala"))
-        const upComingCometitions = KeralaCompetitions.filter((competition: any) => new Date(competition.start_date) > new Date())
+        const upComingCometitions = KeralaCompetitions.filter((competition: any) => new Date(competition.start_date) > new Date()).reverse()
         const pastCometitions = KeralaCompetitions.filter((competition: any) => new Date(competition.start_date ) <= new Date())
         return NextResponse.json({ upcomingCompetitions: upComingCometitions, pastCompetitions: pastCometitions })
     } catch (error) {
